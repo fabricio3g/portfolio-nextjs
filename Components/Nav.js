887bf81fb36f14c4312556fdd84d63878a8627d2
fabricio3g <fabricio.3g@gmail.com>
2022-03-23 03:14:44 -0300

    Update Nav.js

--- Components/Nav.js
+++ Components/Nav.js
@@ -12,7 +12,7 @@ const Nav = ({position, path}) => {
       <Navbar fixed={`top `} className={`${ position ? 'nav-bg': ''}`}>
         <Container>
           <Navbar.Brand href="/">
-            <Link href={'/'}><span className="dot">
+            <Link href={'/'}><span className="dot" id={path === '/aboutme' & !position ? 'dot-white' : ''}>
               <span className="inner_dot"></span>
             </span>
             </Link> 
@@ -20,13 +20,13 @@ const Nav = ({position, path}) => {
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
             <Link href={'/'}>
-              <Button className={path === '/' ? Style.active : Style.btn }>Home</Button>
+              <Button className={path === '/' ? Style.active : Style.btn } id={path === '/aboutme' & !position ? 'text-white' : ''}>Home</Button>
             </Link>
             <Link href={'/portfolio'}>
-              <Button className={path === '/portfolio' ? Style.active : Style.btn }>Portfolio</Button>
+              <Button className={path === '/portfolio' ? Style.active : Style.btn } id={path === '/aboutme' & !position ? 'text-white' : ''}>Portfolio</Button>
             </Link>
             <Link href={'/blog'}>
-              <Button className={path === '/blog' ? Style.active : Style.btn }>Blog</Button>
+              <Button className={path === '/blog' ? Style.active : Style.btn } id={path === '/aboutme' & !position ? 'text-white' : ''} >Blog</Button>
             </Link>
             <Link href={'/aboutme'}>
               <Button className={path === '/aboutme' ? Style.active : Style.btn }>About</Button>
