c4babdecef2a82ea44d9227e21fe716e39dfceef
fabricio3g <fabricio.3g@gmail.com>
2022-03-23 03:14:41 -0300

    Update Posts.js

--- Components/Posts.js
+++ Components/Posts.js
@@ -61,6 +61,45 @@ const Posts = ()=> {
         </Card.Body>
         </Card>
       </Col>
+      <Col>
+        <Card className="mx-auto shadow ">
+        <Card.Body>
+          <h5>Build web3 aplication</h5>
+          <p>react-lazy-load-image-component is used to lazy load React components and images, it boasts of support for intersection observer which determines when an element leaves and enters a viewport and it’s also compatible with server-side rendering (SSR).</p>
+          <p className="text-muted my-1">12-03-2022</p>
+          <br/>
+          <Button className={`${Style.btn1}`}>
+            Read
+          </Button>
+        </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="mx-auto shadow ">
+        <Card.Body>
+          <h5>Build web3 aplication</h5>
+          <p>react-lazy-load-image-component is used to lazy load React components and images, it boasts of support for intersection observer which determines when an element leaves and enters a viewport and it’s also compatible with server-side rendering (SSR).</p>
+          <p className="text-muted my-1">12-03-2022</p>
+          <br/>
+          <Button className={`${Style.btn1}`}>
+            Read
+          </Button>
+        </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="mx-auto shadow ">
+        <Card.Body>
+          <h5>Build web3 aplication</h5>
+          <p>react-lazy-load-image-component is used to lazy load React components and images, it boasts of support for intersection observer which determines when an element leaves and enters a viewport and it’s also compatible with server-side rendering (SSR).</p>
+          <p className="text-muted my-1">12-03-2022</p>
+          <br/>
+          <Button className={`${Style.btn1}`}>
+            Read
+          </Button>
+        </Card.Body>
+        </Card>
+      </Col>
     </Row>
   
   </Container>
