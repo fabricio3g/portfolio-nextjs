a3747e5050bf62dd1b0a8e311a9fbe4311d8334c
fabricio3g <fabricio.3g@gmail.com>
2022-03-24 02:29:11 -0300

    small changes

--- Components/PostContent.js
+++ Components/PostContent.js
@@ -1,19 +1,41 @@
-import { Container, Stack } from "react-bootstrap"
-
+import { Container, Stack } from "react-bootstrap";
 
 const PostContent = () => {
-    return(
-        <Container className='mt-5 pt-5'>
-            <Stack direction="vertical" className='shadow-sm rounded p-5 mx-auto w-85'>
-                    <h3>Build a web3 app</h3>
-                    <div className="mb-3">
-                        <small className="text-muted">20-03-2022</small><small className="text-muted m-3">5 min read</small>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque voluptates aspernatur vero consequuntur assumenda inventore! Ducimus hic amet, vero, porro, ipsum consequuntur animi quas omnis asperiores praesentium itaque inventore quo.</p>
-            </Stack>
-        </Container>
-    )
-}
+  return (
+    <Container className="mt-5 pt-5">
+      <Stack
+        direction="vertical"
+        className="shadow rounded p-3 mx-auto"
+      >
+              <div className="mb-3">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4E03AQFSTGYCrlFFtA/profile-displayphoto-shrink_800_800/0/1638233773279?e=1653523200&v=beta&t=YGUvtuX7m8r4ev1aErJ9Yc303UrJxJfYc8LfJkYg0Pg"
+            alt=""
+            width="32"
+            height="32"
+            className="rounded-circle me-2"
+          />
+          <strong>Fabricio</strong>
+        </div>
+        <h3>Build a web3 app</h3>
+       
+        <div className="mb-3">
+    
+          <small className="text-muted">20-03-2022</small>
+          <small className="text-muted m-3">5 min read</small>
+         
+          <hr />
+        </div>
 
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          voluptates aspernatur vero consequuntur assumenda inventore! Ducimus
+          hic amet, vero, porro, ipsum consequuntur animi quas omnis asperiores
+          praesentium itaque inventore quo.
+        </p>
+      </Stack>
+    </Container>
+  );
+};
 
-export default PostContent
+export default PostContent;
