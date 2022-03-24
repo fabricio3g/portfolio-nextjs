8948b0fcc6f51fe076da7025b44be20787e52f09
fabricio3g <fabricio.3g@gmail.com>
2022-03-24 02:29:00 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import '../styles/variables-white.css'
+
 function MyApp({ Component, pageProps }) {
+  
   return <Component {...pageProps} />
 }
 
