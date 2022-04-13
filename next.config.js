045210532c74b66e97cd754acdcd1fce6d41536f
fabricio3g <fabricio.3g@gmail.com>
2022-04-13 03:55:17 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.imgur.com', 'www.upwork.com', 'socialify.git.ci'],
+  },
 }
 
 module.exports = nextConfig
