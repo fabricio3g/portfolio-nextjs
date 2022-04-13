3f581318f711caa2bc85c8d81476f3eacb0e684c
fabricio3g <fabricio.3g@gmail.com>
2022-04-13 09:58:16 -0300

    Update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,23 @@
 import Layout from "../components/Layout"
 import Work from "../components/Work"
 import {data} from '../test/work'
-
+import {  useContext } from "react";
+import {ThemeContext} from '../utils/contextTheme'
+import { Container } from "react-bootstrap";
 const portfolio = () =>{
+    const { themeName } = useContext(ThemeContext);
+    let themeChange = (themeName === 'DARK')
     return(
         <Layout>
+            <>
+            <div className="mt-5 ">
+
+            </div>
+            <Container fluid className={` mt-2 ${themeChange ? 'bg-gray-0 ' : 'bg-white'} `}>
+            
             <Work data={data}/>
+            </Container>
+            </>
         </Layout>
     )
 } 
