eed1adaf46da02342625e39b447e24c7119ecaf8
fabricio3g <fabricio.3g@gmail.com>
2022-04-13 03:22:18 -0300

    Update work.js

--- test/work.js
+++ test/work.js
@@ -1,46 +1,41 @@
 
-
+https://i.imgur.com/t3YVzGj.png
 module.exports = {
     data:   [
         {
             "title": "Verifiable Education",
-            "description": "Components mad",
-            "github": "https://www.google.com",
-            "live": "https://www.google.com"
+            "description": "React components made for a client, Is an aplication that use blockchain to validate certs",
+            "img": "https://i.imgur.com/t3YVzGj.png",
+            "github": "",
+            "live": ""
+          },
+        {
+            "title": "Crypto Dashboard",
+            "description": "Crypto dashboard with Vue + Typescript, made for a client at Upwork",
+            "img": "https://www.upwork.com/att/download/portfolio/persons/uid/1274940380159815680/profile/projects/files/23945e3e-237c-4860-99d1-24ce4ecb5cfc",
+            "github": "",
+            "live": ""
+          },
+        {
+            "title": "Personal Portfolio",
+            "description": "This exact website, made with NextJS and Love",
+            "img": "https://socialify.git.ci/fabricio3g/portfolio-nextjs/image?font=Raleway&language=1&name=1&owner=1&stargazers=1&theme=Dark",
+            "github": "https://github.com/fabricio3g/portfolio-nextjs",
+            "live": "https://portfolio-nextjs-orcin.vercel.app/"
           },
           {
-            "title": "Verifiable Education",
-            "description": "Components mad",
-            "github": "https://www.google.com",
-            "live": "https://www.google.com"
-          }
-          ,
+            "title": "Tab Tracker",
+            "description": "A Tracker for guitar tabs made with Vue and Express",
+            "img": "https://socialify.git.ci/fabricio3g/tab-tracker/image?language=1&name=1&owner=1&stargazers=1&theme=Dark",
+            "github": "https://github.com/fabricio3g/tab-tracker",
+            "live": ""
+          },
           {
-            "title": "Verifiable Education",
-            "description": "Components mad",
-            "github": "https://www.google.com",
-            "live": "https://www.google.com"
-          }
-          ,
-          {
-            "title": "Verifiable Education",
-            "description": "Components mad",
-            "github": "https://www.google.com",
-            "live": "https://www.google.com"
-          }
-          ,
-          {
-            "title": "Verifiable Education",
-            "description": "Components mad",
-            "github": "https://www.google.com",
-            "live": "https://www.google.com"
-          }
-          ,
-          {
-            "title": "Verifiable Education",
-            "description": "Components mad",
-            "github": "https://www.google.com",
-            "live": "https://www.google.com"
+            "title": "Wordle Clone",
+            "description": "Simple Wordle clone with vanilla js and Tailwind",
+            "img": "https://socialify.git.ci/fabricio3g/wordle-clone/image?forks=1&language=1&name=1&owner=1&stargazers=1&theme=Light",
+            "github": "https://github.com/fabricio3g/wordle-clone",
+            "live": "https://fabricio3g.github.io/wordle-clone/"
           }
     ]
 }
