3b2dfb0613dc8af05accdf9f4f709bfb72952583
fabricio3g <fabricio.3g@gmail.com>
2022-04-13 00:20:31 -0300

    test data

---
+++ test/work.js
@@ -0,0 +1,26 @@
+
+
+module.exports = {
+    data:   [
+        {
+            title: 'Verifiable Education',
+            description: 'Build React components and Pages for a client at UpWork',
+            img: ''
+        },  {
+            title: 'Crypto dashboard',
+            description: 'Builded with Vue for a client at UpWork',
+            img: 'ttps://media-exp1.licdn.com/dms/image/C4E03AQFSTGYCrlFFtA/profile-displayphoto-shrink_800_800/0/1638233773279?e=1653523200&v=beta&t=YGUvtuX7m8r4ev1aErJ9Yc303UrJxJfYc8LfJkYg0Pg'
+        },
+        {
+            title: 'Tab Tracker',
+            description: 'A tab tracker made with Vue and Node for the backend',
+            img: 'https://www.upwork.com/att/download/portfolio/persons/uid/1274940380159815680/profile/projects/files/f184e5d8-cb38-451d-afbd-4d8918234abb'
+        },
+        {
+            title: 'Twitter clone',
+            description: 'Just a simple front end clone made with React and Styled Components',
+            img: 'https://www.upwork.com/att/download/portfolio/persons/uid/1274940380159815680/profile/projects/files/0fcc5c62-762c-433d-889b-a25325e1d9a9'
+        }
+    ]
+}
+
