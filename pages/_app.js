3284da6d717a7c62a9771ef9789c508b1670cb42
fabricio3g <fabricio.3g@gmail.com>
2022-04-13 00:19:50 -0300

    Dynamic Title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 import '../styles/variables-white.css'
+import Head from 'next/head'
+import { useRouter } from 'next/router';
+
+import { ThemeProvider } from '../utils/contextTheme';
 
 function MyApp({ Component, pageProps }) {
+
+  const { asPath } = useRouter();
+
+  const dynamicTitle = ()=>{
+    const names = ['- Home', '- Porfolio', '- Blog', '- About']
+    switch(asPath){
+      case '/':
+        return names[0]
+        break;
+      case '/portfolio':
+        return names[1]
+        break;
+      case '/blog':
+          return names[2]
+          break;
+      case '/aboutme':
+            return names[3]
+            break;
+      default:
+          return ''
+          break;
+    }
+  }
+  return(
+    
+    <ThemeProvider>
+      <Head>
+        <title>{`.Fabricio ${dynamicTitle()}`}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    <Component {...pageProps} />
+    </ThemeProvider>
+  )
   
-  return <Component {...pageProps} />
 }
 
 export default MyApp
