4aa5c9fa69c1ac28cc7a95ec269fce35afdcf7dc
fabricio3g <fabricio.3g@gmail.com>
2022-04-15 12:44:41 -0300

    placeholder for loading component

--- components/PostContent.js
+++ components/PostContent.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { Container, Stack } from "react-bootstrap";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import ReactMarkdown from 'react-markdown'
 import { useReadingTime } from "react-reading-time-estimator";
 const PostContent = ({post}) => {
   const [markdown, setMarkdown] = useState([])
   const [loading, setLoading] = useState(true)
   const md = post.md
+  function timeOutLoading(){
+    setLoading(false)
+  }
   useEffect(()=>{
     fetch(md)
       .then( res => res.text())
@@ -17,15 +21,41 @@ const PostContent = ({post}) => {
       ).catch(err => {throw err})
       .finally(()=>
       {
-        setLoading(false)
+       setTimeout(timeOutLoading , 3000)
       }
       )
   },[])
-  console.log(markdown)
  
   return (
+
+    
     <Container className="mt-5 pt-5 mb-5">
-      <Stack
+      {
+      loading ? (
+        <Container  className=" border rounded p-5 shadow">
+
+
+     <svg className="mx-auto" width="50" height="50" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g id="loader">
+    <animateTransform 
+
+           attributeName="transform" 
+           attributeType="XML"
+           type="rotate"
+           from="0 50 50"
+           to="360 50 50" 
+           dur="1s"
+           begin="0s"
+           repeatCount="indefinite"
+           restart="always" 
+           />
+    <path className="a" opacity="0.2" fillRule="evenodd" clipRule="evenodd" d="M50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100ZM50 90C72.0914 90 90 72.0914 90 50C90 27.9086 72.0914 10 50 10C27.9086 10 10 27.9086 10 50C10 72.0914 27.9086 90 50 90Z" fill="#3aa19d"/>
+    <path className="b" fillRule="evenodd" clipRule="evenodd" d="M100 50C100 22.3858 77.6142 0 50 0V10C72.0914 10 90 27.9086 90 50H100Z" fill="#6b12fe"/>
+    </g>
+</svg>
+
+</Container>
+) : (<Stack
         direction="vertical"
         className="shadow rounded p-3 mx-auto"
       >
@@ -52,7 +82,9 @@ const PostContent = ({post}) => {
         <ReactMarkdown>
           {markdown}
         </ReactMarkdown>
-      </Stack>
+      </Stack>)
+      }
+      
     </Container>
   );
 };
--- components/Posts.js
+++ components/Posts.js
@@ -37,7 +37,29 @@ const Posts = ()=> {
 
     <Row xs={1} md={3} className="gap-3 justify-content-center mx-auto mt-5">
       {
-        loading ? (<div>Loading...</div>) :
+        loading ? (<Container  className=" border rounded p-5 shadow">
+
+
+        <svg className="mx-auto" width="50" height="50" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+     <g id="loader">
+       <animateTransform 
+   
+              attributeName="transform" 
+              attributeType="XML"
+              type="rotate"
+              from="0 50 50"
+              to="360 50 50" 
+              dur="1s"
+              begin="0s"
+              repeatCount="indefinite"
+              restart="always" 
+              />
+       <path className="a" opacity="0.2" fillRule="evenodd" clipRule="evenodd" d="M50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100ZM50 90C72.0914 90 90 72.0914 90 50C90 27.9086 72.0914 10 50 10C27.9086 10 10 27.9086 10 50C10 72.0914 27.9086 90 50 90Z" fill="#3aa19d"/>
+       <path className="b" fillRule="evenodd" clipRule="evenodd" d="M100 50C100 22.3858 77.6142 0 50 0V10C72.0914 10 90 27.9086 90 50H100Z" fill="#6b12fe"/>
+       </g>
+   </svg>
+   
+   </Container>) :
         (blog.map((data , i)=>(
           <Col key={i}>
         <Card  className="mx-auto shadow  ">
