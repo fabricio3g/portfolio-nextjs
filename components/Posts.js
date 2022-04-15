2907f7b26ef305d38ab46b3cd90710f67438d059
fabricio3g <fabricio.3g@gmail.com>
2022-04-15 01:55:04 -0300

    Update Posts.js

--- components/Posts.js
+++ components/Posts.js
@@ -1,48 +1,66 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap"
 import Style from "../styles/Button.module.css";
-import {  useContext } from "react";
+import {  useContext, useEffect, useState } from "react";
 import {ThemeContext} from '../utils/contextTheme'
+import Link from 'next/link'
+
+
 
 const Posts = ()=> {
+  const fakeData = [1,1,1,2]
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    fetch('https://raw.githubusercontent.com/fabricio3g/blog-markdown/main/blog.json')
+      .then( res => res.json())
+      .then(data => 
+        {
+          setPosts(data)
+        }  
+      ).catch(err => {throw err})
+      .finally(()=>
+      {
+        setLoading(false)
+      }
+      )
+  },[])
+
+  const { blog } = posts
   const { themeName } = useContext(ThemeContext);
   let themeChange = (themeName === 'DARK')
 
     return(
-      <>
-    <Container  className={`p-3 mt-2 ${themeChange && 'bg-gray-0 text-white'}`} >
+      
+    <Container  className={`pb-5 mt-2 ${themeChange && ' bg-gray-0 text-white '}`} >
     <h1 className='mx-5 font-weight-bold pt-5'>Blog.</h1>
-    <Row xs={1} md={4} className="gap-4 justify-content-center mx-auto mt-5">
-      <Col>
-        <Card className="mx-auto shadow ">
+
+    <Row xs={1} md={3} className="gap-3 justify-content-center mx-auto mt-5">
+      {
+        loading ? (<div>Loading...</div>) :
+        (blog.map((data , i)=>(
+          <Col key={i}>
+        <Card  className="mx-auto shadow  ">
         <Card.Body className='text-black'>
-          <h5>Build web3 aplication</h5>
+          <h5>{data.title}</h5>
           <p className="text-muted ">react-lazy-load-image-component is used to lazy load React components and images, it boasts of support for intersection observer which determines when an element leaves and enters a viewport and it’s also compatible with server-side rendering (SSR).</p>
           <p className="text-muted my-1">12-03-2022</p>
           <br/>
-          <Button className={`${Style.btn1}`}>
+          <Link passHref href={`blog/${data.id.toString()}`}>
+          <Button  className={`${Style.btn1}`}>
             Read
           </Button>
+          </Link>
         </Card.Body>
         </Card>
-      </Col>
-      <Col>
-        <Card className="mx-auto shadow ">
-        <Card.Body>
-          <h5 className='text-black'>Build web3 aplication</h5>
-          <p className="text-muted ">react-lazy-load-i and enters a viewport and it’s also compatible with server-side rendering (SSR).</p>
-          <p className="text-muted my-1">12-03-2022</p>
-          <br/>
-          <Button className={`${Style.btn1}`}>
-            Read
-          </Button>
-        </Card.Body>
-        </Card>
-      </Col>
+      </Col>)
+        ))
+      }
       
     </Row>
   
   </Container>
-  </>
+
     )
 }
 
