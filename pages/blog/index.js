d8e507aba79c43647fc80fe12bb31ac3e58335d9
fabricio3g <fabricio.3g@gmail.com>
2022-04-15 01:54:35 -0300

    Create index.js

---
+++ pages/blog/index.js
@@ -0,0 +1,23 @@
+import Posts from "../../components/Posts"
+import Layout from '../../components/Layout'
+import { Container } from "react-bootstrap"
+import {  useContext } from "react";
+import {ThemeContext} from '../../utils/contextTheme'
+
+function Blog(){
+    const { themeName } = useContext(ThemeContext);
+    let themeChange = (themeName === 'DARK')
+    return(
+     
+            <Layout>
+                <div className="mt-5 ">
+
+                </div>
+                <Container fluid className={` mt-2 max-h-full ${themeChange && 'bg-gray-0 text-white'}`}>
+                <Posts/>
+                </Container>
+            </Layout>
+    )
+}
+
+export default Blog
