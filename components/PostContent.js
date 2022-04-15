46a86e10648f4c17c9510242747549e4c2f25ba3
fabricio3g <fabricio.3g@gmail.com>
2022-04-15 01:55:08 -0300

    Update PostContent.js

--- components/PostContent.js
+++ components/PostContent.js
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import { Container, Stack } from "react-bootstrap";
-
-const PostContent = () => {
+import ReactMarkdown from 'react-markdown'
+import { useReadingTime } from "react-reading-time-estimator";
+const PostContent = ({post}) => {
+  const [markdown, setMarkdown] = useState([])
+  const [loading, setLoading] = useState(true)
+  const md = post.md
+  useEffect(()=>{
+    fetch(md)
+      .then( res => res.text())
+      .then(data => 
+        {
+          console.log(data)
+          setMarkdown(data)
+        }  
+      ).catch(err => {throw err})
+      .finally(()=>
+      {
+        setLoading(false)
+      }
+      )
+  },[])
+  console.log(markdown)
+ 
   return (
-    <Container className="mt-5 pt-5">
+    <Container className="mt-5 pt-5 mb-5">
       <Stack
         direction="vertical"
         className="shadow rounded p-3 mx-auto"
       >
-              <div className="mb-3">
+              <div className="pb-3">
           <img
             src="https://media-exp1.licdn.com/dms/image/C4E03AQFSTGYCrlFFtA/profile-displayphoto-shrink_800_800/0/1638233773279?e=1653523200&v=beta&t=YGUvtuX7m8r4ev1aErJ9Yc303UrJxJfYc8LfJkYg0Pg"
             alt=""
@@ -17,22 +39,19 @@ const PostContent = () => {
           />
           <strong>Fabricio</strong>
         </div>
-        <h3>Build a web3 app</h3>
+        <h3>{post.title}</h3>
        
         <div className="mb-3">
     
           <small className="text-muted">20-03-2022</small>
-          <small className="text-muted m-3">5 min read</small>
+          <small className="text-muted m-3"></small>
          
           <hr />
         </div>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
-          voluptates aspernatur vero consequuntur assumenda inventore! Ducimus
-          hic amet, vero, porro, ipsum consequuntur animi quas omnis asperiores
-          praesentium itaque inventore quo.
-        </p>
+        <ReactMarkdown>
+          {markdown}
+        </ReactMarkdown>
       </Stack>
     </Container>
   );
