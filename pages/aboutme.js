7b267a866d14355f5d043b50c6c5aaaf266e1614
fabricio3g <fabricio.3g@gmail.com>
2022-05-01 21:11:23 -0300

    Update aboutme.js

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,51 +1,12 @@
 import { Container } from "react-bootstrap";
 import Layout from "../components/Layout";
-import { BiLinkExternal, BiDownArrowAlt } from "react-icons/bi";
+import { BiDownArrowAlt } from "react-icons/bi";
 import { Button } from "react-bootstrap";
 import Style from "../styles/Button.module.css";
-import { useEffect, useState } from "react";
+import Certfs from '../components/Certfs'
 
 const AboutMe = () => {
       
-      function _arrayBufferToBase64( buffer ) {
-        // Thanks to: 
-        // https://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
-        let binary = '';
-        let bytes = new Uint8Array( buffer );
-        let len = bytes.byteLength;
-        for (let i = 0; i < len; i++) {
-            binary += String.fromCharCode( bytes[ i ] );
-        }
-        return window.btoa( binary );
-    }
-
-    const [pdf, setPdf] = useState(null)
-    const [loading, setLoading] = useState(true)
-    
-    useEffect(()=>{
-      fetch('https://raw.githubusercontent.com/fabricio3g/personal-portfolio/main/old_desing/assets/certs/CertificadoDeFinalizacion_Become%20a%20Software%20Developer.pdf')
-        .then( res => res.arrayBuffer())
-        .then(data => 
-          {
-            const base64 =_arrayBufferToBase64(data)
-            setPdf(base64)
-          }  
-        ).catch(err => {throw err})
-        .finally(()=>
-        {
-          setLoading(false)
-        }
-        )
-    },[])
-
-
-
-
-
-
-
-
-
 
   return (
     <Layout>
@@ -69,23 +30,14 @@ const AboutMe = () => {
           </a>
         </Container>
       </Container>
+      <Container>
+    
+      </Container>
       <Container id="studies" className="mt-5 pt-5 d-flex flex-column align-items-center">
         <h4 className="text-center my-2 ">Courses certifications</h4>
   
-          <div role="button" className={`d-flex justify-content-between pe-auto w-100  p-2 rounded shadow my-2 border`}><span>Javascript Algorithms and Data Structures - FCC</span> <BiDownArrowAlt size={26}/></div>
          
-
-        {
-          loading ? "" :  (
-           <Container className="p-2 border rounded"> 
-            <iframe 
-  title="frame"
-  width="100%"
-  height="700px"
-  src={`data:application/pdf;base64,${pdf}`} />
-  </Container>
-          )
-        }
+        <Certfs title={'Become a Software Developer'} url={"https://raw.githubusercontent.com/fabricio3g/personal-portfolio/main/old_desing/assets/certs/CertificadoDeFinalizacion_Become%20a%20Software%20Developer.pdf"}/>
           
       </Container>
     </Layout>
