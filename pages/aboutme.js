2d669c034d46f92dde68c3d9482b02ab8097aa65
fabricio3g <fabricio.3g@gmail.com>
2022-05-02 01:20:22 -0300

    Text chaged

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -12,15 +12,13 @@ const AboutMe = () => {
     <Layout>
       <Container fluid className="bg-black pt-2 pb-5">
         <Container className="d-flex flex-column text-white mt-5" >
-          <h1 className="about-title border-0  text-center mt-5 text-white">About Me</h1>
-          <h2 className="text-center aboutme-text mb-3">
+          
+          <h2 className=" text-center aboutme-text  my-5">
             Hello 👋 , I&apos;m Fabricio
           </h2>
-          <p className="text-center ">
-            A Argentinian student at the National University of Rosario of Medical Science,
-            and an a technology enthusiast and dog lover
-          </p>
-          <h6 className="text-center pt-3">
+          <p className="text-center lead">
+          A Argentinian student at the National University of Rosario of Medical Science, and a technology enthusiast and dog lover 🐕</p>
+          <h6 className="text-center">
             You can find more about my education below
           </h6>
           <a className="mx-auto" href="#studies">
@@ -30,14 +28,11 @@ const AboutMe = () => {
           </a>
         </Container>
       </Container>
-      <Container>
-    
-      </Container>
       <Container id="studies" className="mt-5 pt-5 d-flex flex-column align-items-center">
         <h4 className="text-center my-2 ">Courses certifications</h4>
   
          
-        <Certfs title={'Become a Software Developer'} url={"https://raw.githubusercontent.com/fabricio3g/personal-portfolio/main/old_desing/assets/certs/CertificadoDeFinalizacion_Become%20a%20Software%20Developer.pdf"}/>
+        <Certfs title={'Become a Software Developer - Linkedin Learning'} url={"https://raw.githubusercontent.com/fabricio3g/personal-portfolio/main/old_desing/assets/certs/CertificadoDeFinalizacion_Become%20a%20Software%20Developer.pdf"}/>
           
       </Container>
     </Layout>
